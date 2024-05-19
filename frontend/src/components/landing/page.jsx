26ccbe7a8116200e8b49a4bd4aabf5630403fbeb
im-calvin <kelvinwong0519@gmail.com>
2024-05-19 04:53:27 -0700

    add copy email. make onboarding work

--- frontend/src/components/landing/page.jsx
+++ frontend/src/components/landing/page.jsx
@@ -1,114 +1,93 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import mockData from '../../data/mockjobs';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import mockData from "../../data/mockjobs";
 
-import mail from '../../assets/'
+// import mail from '../assets/'
 
 function LoginPage() {
-    const history = useNavigate();  
+  const navigate = useNavigate();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
-    async function login(formData) {
-        try {
-            const firstName = formData.get("first");
-            const lastName = formData.get("last");
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-            console.log("Incoming User: ", firstName, lastName);
+    ("use server");
+    try {
+      console.log("Incoming User: ", firstName, lastName);
 
-            const user = firstName.toLowerCase() + lastName.toLowerCase();
-            const response = await fetch(`http://localhost:5000/api/emails?username=${user}`)
-            
-            if (!response.ok) {
-                throw new Error("Failed to fetch data");
-            }
-            const data = await response.json();
-            console.log(data);
+      const user = firstName.toLowerCase().trim() + lastName.toLowerCase().trim();
+      const response = await fetch(`http://localhost:5000/api/emails?username=${user}`);
 
-            if (data.length === 0) { // redirect user to email address screen
-                const promptData = {
-                    firstName,
-                    lastName
-                };
-                history.push({
-                    pathname: '/register',
-                    state: promptData
-                });
-            } else {
-                history.push({ // redirect user to main dashboard with data
-                    pathname: '/home',
-                    state: data
-                });
-            }
-        } catch {
-            alert("Failed to log in");
-        }
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await response.json();
+      console.log(data);
 
-
-
+      if (data.length === 0) {
+        // redirect user to email address screen
+        const promptData = {
+          firstName,
+          lastName,
+        };
+        navigate("/register", {
+          state: promptData,
+        });
+      } else {
+        // redirect user to main dashboard with data
+        navigate("/home", { state: data });
+      }
+    } catch {
+      alert("Failed to log in");
     }
-    return (
-        <>
-            <div className='pt-32'>
-            <div className='w-3/4 border-2 rounded-lg px-20 border-gray-200 m-auto'>  
-                <div className='mt-20'></div>
+  }
+  return (
+    <>
+      <div className="pt-32">
+        <div className="w-3/4 border-2 rounded-lg px-20 border-gray-200 m-auto">
+          <div className="mt-20"></div>
 
-                <div className='w-1/2 h-fit grid grid-rows-3'>
-                    <div className='container mt-20'>
-                    <h1 className='text-6xl'>Create an account</h1>
-                    <h3> Already have an account? Log in</h3>
-                    </div>
-
-                    <div className='flex flex-row mt-10 h-fit w-full gap-4 justify-between'>
-                        <form action={login}>
-                            <div className='w-11/12'>
-                                <h3 className='mb-2'>First Name</h3>
-                                <input name="first" className='h-16 border border-gray-300 rounded-md px-4 pt-5 pb-1'></input>
-                            </div>
-
-                            <div className='w-11/12 h-fit'>
-                                <h3 className='mb-2'> Last Name </h3>
-                                <input name="last" className='h-16 border border-gray-300 rounded-md px-4 pt-5 pb-1'></input>
-                            </div>
-                            <button type="submit">Next</button>
-                        </form>   
-                    </div>
-
-                    {/* <div class='container h-fit flex mt-5 flex-col justify-between'>
-                        <h3> Email address </h3>
-                        <div className='h-16 border w-full border-gray-300 rounded-md px-4 pt-5 pb-1'> 
-                            <div className=''> words </div>
-                        </div>
-                    </div>
-
-                    <div className='flex flex-row w-full gap-4 justify-between'>
-                        <div className='w-11/12'>
-                        <h3 className='mb-2'>Password</h3>
-                        <div className='h-16 border border-gray-300 rounded-md px-4 pt-5 pb-1'> words </div>
-                        </div>
-
-                        <div className='w-11/12'>
-                        <h3 className='mb-2'> Confirm </h3>
-                        <div className='h-16 border border-gray-300 rounded-md px-4 pt-5 pb-1'> words </div>
-                        </div>
-                    </div> */}
-
-                    <div className='flex flex-col justify-bottom'>
-                        <div className='flex flex-row justify-between'>
-                            {/* <h3 className='flex flex-col justify-end'>Log in instead</h3> */}
-
-                            <div className=''>
-                            <button className='border border-gray-300 rounded-md bg-gray-900 px-10 py-3'> <h3 className='font-bold'>Create Account</h3></button>
-                            </div>
-                        </div>
-                    </div>
-
-                
-                </div>
-                </div>
-
-                <div className='pb-20'></div>
+          <div className="w-1/2 h-fit grid grid-rows-3">
+            <div className="container mt-20">
+              <h1 className="text-6xl">Create an account</h1>
+              <h3> Already have an account? Log in</h3>
             </div>
-        </>
-    );
-};
+
+            <div className="flex flex-row mt-10 h-fit w-full gap-4 justify-between">
+              <form onSubmit={handleSubmit}>
+                <div className="w-11/12">
+                  <h3 className="mb-2">First Name</h3>
+                  <input
+                    name="first"
+                    className="h-16 border border-gray-300 rounded-md px-4 pt-5 pb-1"
+                    value={firstName}
+                    onChange={(event) => setFirstName(event.target.value)}></input>
+                </div>
+
+                <div className="w-11/12 h-fit">
+                  <h3 className="mb-2"> Last Name </h3>
+                  <input
+                    name="last"
+                    className="h-16 border border-gray-300 rounded-md px-4 pt-5 pb-1"
+                    value={lastName}
+                    onChange={(event) => setLastName(event.target.value)}></input>
+                </div>
+                <button
+                  type="submit"
+                  className="border border-gray-300 rounded-md bg-gray-900 px-10 py-3">
+                  {" "}
+                  <h3 className="font-bold">Create Account</h3>
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <div className="pb-20"></div>
+      </div>
+    </>
+  );
+}
 
 export default LoginPage;
--- frontend/src/components/register/page.jsx
+++ frontend/src/components/register/page.jsx
@@ -1,39 +1,58 @@
-import React from 'react';
+import React from "react";
+import { useLocation } from "react-router-dom";
 
-function RegisterPage({data}) {  
-    return (
-        <>
-            <div className='pt-32'>
-            <div className='w-3/4 h-fit border-2 rounded-lg px-20 border-gray-200 m-auto'>  
-                <div className='mt-20'></div>
+function RegisterPage() {
+  const location = useLocation();
+  const state = location.state;
+  const username = state.firstName + state.lastName;
 
-                <div className=' bg-red-500 rounded-full size-20'> </div>
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(username + "@jobjug.co");
+      alert("Copied to clipboard!");
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  };
+  return (
+    <>
+      <div className="pt-32">
+        <div className="w-3/4 h-fit border-2 rounded-lg px-20 border-gray-200 m-auto">
+          <div className="mt-20"></div>
 
-                <div className='w-4/5 grid grid-rows-2'>
-                    <div className='container mt-20'>
-                    <h3> Don't lose it!</h3>
-                    <h1 className='text-6xl'>Your new email is</h1>
-                    </div>
+          <div className=" bg-red-500 rounded-full size-20"> </div>
 
-                <div className='container mt-5' >
-
-                <h3 className='mb-2'>First Name</h3>
-
-                    <div className='flex w-full gap-4 mb-20'>
-                        <div className='border w-3/4 h-16 border-gray-300 rounded-md px-4 pt-5 pb-1'> words </div>
-                        <button className='border w-1/4 h-16 border-gray-300 bg-black rounded-md px-4 pt-3 pb-1'> <h3 className='text-white'> Copy Email </h3> </button>
-                        <button className='border w-1/4 h-16 border-gray-300 bg-black rounded-md px-4 pt-3 pb-1'> <h3 className='text-white'> Next </h3> </button>
-                    </div>
-                </div>
-
-                
-                </div>
-                </div>
-
-                <div className='pb-20'></div>
+          <div className="w-4/5 grid grid-rows-2">
+            <div className="container mt-20">
+              <h3> Don't lose it!</h3>
+              <h1 className="text-6xl">Your new email is</h1>
             </div>
-        </>
-    );
-};
+
+            <div className="container mt-5">
+              <div className="flex w-full gap-4 mb-20">
+                <div className="border w-3/4 h-16 border-gray-300 rounded-md px-4 pt-5 pb-1">
+                  {username}
+                  {"@jobjug.co"}
+                </div>
+                <button
+                  className="border w-1/4 h-16 border-gray-300 bg-black rounded-md px-4 pt-3 pb-1"
+                  onClick={handleCopy}>
+                  {" "}
+                  <h3 className="text-white"> Copy Email </h3>{" "}
+                </button>
+                <button className="border w-1/4 h-16 border-gray-300 bg-black rounded-md px-4 pt-3 pb-1">
+                  {" "}
+                  <h3 className="text-white"> Go Home </h3>{" "}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="pb-20"></div>
+      </div>
+    </>
+  );
+}
 
 export default RegisterPage;
