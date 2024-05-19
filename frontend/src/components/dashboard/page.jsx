2a0e3acc4711f8d38063afd696071da0e089847f
Vince <112585869+vinn03@users.noreply.github.com>
2024-05-19 08:36:16 -0700

    removed redundant html tags

--- frontend/src/components/dashboard/page.jsx
+++ frontend/src/components/dashboard/page.jsx
@@ -11,75 +11,69 @@ function Dashboard({goodStuff, badStuff, awaiting}) {
     return (
         <div className="flex flex-col justify-start m-auto h-lvh w-4/5">
             <table className=''>
-                <thead className=''>
-                </thead>
-                <tbody>
-
+                <tr className='flex flex-row'>
+                    <td className='h-fit bg-gray-200 rounded-md mx-2 flex-col flex w-full justify-start'>
+                    {/* /*className=" m-auto h-fit flex flex-col bg-gray-300 w-11/12">(/) */}
+                    <th className='w-full mb-5 bg-gray-900 rounded-md py-3 px-4 shadow-md'><h1 className="text-white">Awaiting Application</h1></th>
+                        {awaiting.map(job => (
+                            <div className="mb-4 mx-2.5 w-11/12 border border-gray-300 bg-contain rounded-md shadow-md overflow-hidden" style={{backgroundColor: statusColours[job.status]}} key={job.id}>
+                                <div className='flex-row flex'>
+                                    <span className='h-full w-1/12' style={{backgroundColor: statusColours[job.status]}}>
+                                    </span>
+                                    <span className="h-full w-11/12 bg-white"> 
+                                        <div className='flex flex-row px-3 py-2'>
+                                                <div className='container flex flex-col justify-center'>
+                                                    <div><h1>{job.title}</h1></div>
+                                                    <div><h5>{job.company}</h5></div>
+                                                </div>
+                                        </div>
+                                    </span>
+                                </div>
+                            </div>
+                        ))}
+                    </td>
                     
-                    <tr className='flex flex-row'>
-                        <td className='h-fit bg-gray-200 rounded-md mx-2 flex-col flex w-full justify-start'>
-                        {/* /*className=" m-auto h-fit flex flex-col bg-gray-300 w-11/12">(/) */}
-                        <th className='w-full mb-5 bg-gray-900 rounded-md py-3 px-4 shadow-md'><h1 className="text-white">Awaiting Application</h1></th>
-                            {awaiting.map(job => (
-                                <div className="mb-4 mx-2.5 w-11/12 border border-gray-300 bg-contain rounded-md shadow-md overflow-hidden" style={{backgroundColor: statusColours[job.status]}} key={job.id}>
-                                    <div className='flex-row flex'>
-                                        <span className='h-full w-1/12' style={{backgroundColor: statusColours[job.status]}}>
-                                        </span>
-                                        <span className="h-full w-11/12 bg-white"> 
-                                            <div className='flex flex-row px-3 py-2'>
-                                                    <div className='container flex flex-col justify-center'>
-                                                        <div><h1>{job.title}</h1></div>
-                                                        <div><h5>{job.company}</h5></div>
-                                                    </div>
-                                            </div>
-                                        </span>
-                                    </div>
+                    <td className='h-fit mb-auto bg-gray-200 rounded-md mx-2 flex-col flex justify-start w-full'>
+                    <th className='w-full mb-5 bg-gray-900 rounded-md py-3 px-4 shadow-md'><h1 className="text-white">Good stuff</h1></th>
+                        {goodStuff.map(job => (
+                            <div className="mb-4 mx-2.5 w-11/12 border border-gray-300 bg-contain rounded-md shadow-md overflow-hidden" style={{backgroundColor: statusColours[job.status]}} key={job.id}>
+                                <div className='flex-row flex'>
+                                    <span className='h-full w-1/12' style={{backgroundColor: statusColours[job.status]}}>
+                                    </span>
+                                    <span className="h-full w-11/12 bg-white"> 
+                                        <div className='flex flex-row px-3 py-2'>
+
+                                                <div className='container flex flex-col justify-center'>
+                                                    <div><h1>{job.title}</h1></div>
+                                                    <div><h5>{job.company}</h5></div>
+                                                </div>
+                                        </div>
+                                    </span>
                                 </div>
-                            ))}
-                        </td>
-                        
-                        <td className='h-fit mb-auto bg-gray-200 rounded-md mx-2 flex-col flex justify-start w-full'>
-                        <th className='w-full mb-5 bg-gray-900 rounded-md py-3 px-4 shadow-md'><h1 className="text-white">Good stuff</h1></th>
-                            {goodStuff.map(job => (
-                                <div className="mb-4 mx-2.5 w-11/12 border border-gray-300 bg-contain rounded-md shadow-md overflow-hidden" style={{backgroundColor: statusColours[job.status]}} key={job.id}>
-                                    <div className='flex-row flex'>
-                                        <span className='h-full w-1/12' style={{backgroundColor: statusColours[job.status]}}>
-                                        </span>
-                                        <span className="h-full w-11/12 bg-white"> 
-                                            <div className='flex flex-row px-3 py-2'>
-    
-                                                    <div className='container flex flex-col justify-center'>
-                                                        <div><h1>{job.title}</h1></div>
-                                                        <div><h5>{job.company}</h5></div>
-                                                    </div>
-                                            </div>
-                                        </span>
-                                    </div>
+                            </div>
+                        ))}
+                    </td>
+                    
+                    <td className='h-fit bg-gray-200 rounded-md mx-2 flex-col flex justify-start w-full'>
+                    <th className='w-full mb-5 bg-gray-900 rounded-md py-3 px-4 shadow-md'><h1 className="text-white"> Rejections</h1></th>
+                        {badStuff.map(job => (
+                            <div className="mb-4 mx-2.5 w-11/12 border border-gray-300 bg-red-400 bg-contain rounded-md shadow-md overflow-hidden" key={job.id}>
+                                <div className='flex-row flex'>
+                                    <span className='h-full w-1/12 bg-red-400'>
+                                    </span>
+                                    <span className="h-full w-11/12 bg-white"> 
+                                        <div className='flex flex-row px-3 py-2'>
+                                                <div className='container flex flex-col justify-center'>
+                                                    <div><h1>{job.title}</h1></div>
+                                                    <div><h5>{job.company}</h5></div>
+                                                </div>
+                                        </div>
+                                    </span>
                                 </div>
-                            ))}
-                        </td>
-                        
-                        <td className='h-fit bg-gray-200 rounded-md mx-2 flex-col flex justify-start w-full'>
-                        <th className='w-full mb-5 bg-gray-900 rounded-md py-3 px-4 shadow-md'><h1 className="text-white"> Rejections</h1></th>
-                            {badStuff.map(job => (
-                                <div className="mb-4 mx-2.5 w-11/12 border border-gray-300 bg-red-400 bg-contain rounded-md shadow-md overflow-hidden" key={job.id}>
-                                    <div className='flex-row flex'>
-                                        <span className='h-full w-1/12 bg-red-400'>
-                                        </span>
-                                        <span className="h-full w-11/12 bg-white"> 
-                                            <div className='flex flex-row px-3 py-2'>
-                                                    <div className='container flex flex-col justify-center'>
-                                                        <div><h1>{job.title}</h1></div>
-                                                        <div><h5>{job.company}</h5></div>
-                                                    </div>
-                                            </div>
-                                        </span>
-                                    </div>
-                                </div>
-                            ))}
-                        </td>
-                    </tr>
-                </tbody>
+                            </div>
+                        ))}
+                    </td>
+                </tr>
             </table>
         </div>
     )
