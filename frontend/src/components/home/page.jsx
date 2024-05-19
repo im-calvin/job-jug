63488998391239e5e52533dc9bddd70eaf211eb1
im-calvin <kelvinwong0519@gmail.com>
2024-05-18 23:51:48 -0700

    fix merge

--- frontend/src/components/home/page.jsx
+++ frontend/src/components/home/page.jsx
@@ -16,44 +16,41 @@ function HomePage() {
   // awaiting: interviewing (3), received (1), waitlisted (5)
 
   const fetchData = async () => {
-    try {
-      const response = await fetch(`http://localhost:5000/api/emails?username=${user}`);
-      if (!response.ok) {
-        throw new Error("Failed to fetch data");
-      }
-      const data = await response.json();
-      console.log(data);
-
-      // filtering fetched data into three main columns
-      const tempGood = data.filter((job) => job.status === 4);
-      setGoodStuff(tempGood);
-      let tempAwait = data.filter(
-        (job) => job.status === 1 || job.status === 3 || job.status === 5
-      );
-      setAwaiting(tempAwait);
-
-        const currentDate = new Date();
-        const tempGhosted = mockData.filter(job => {
-            const jobDate = new Date(job.date);
-            const diffTime = jobDate.getTime() - currentDate.getTime();
-            const diffDays = Math.abs(Math.round(diffTime / (1000 * 3600 * 24)));
-            console.log("Job ", job.id, ": ", diffDays);
-            return diffDays > 21;
-        });
-        setGhostedJobs(tempGhosted);
-        tempAwait = tempAwait.filter(job => tempGhosted.includes(job) === false);
-        setAwaiting(tempAwait);
-        const tempBad = mockData.filter(job => job.status === 2).concat(tempGhosted)
-        setBadStuff(tempBad);
-
-        const reject = mockData.filter(job => job.status === 2).length;
-        const interview = mockData.filter(job => job.status === 3).length;
-        const offer = mockData.filter(job => job.status === 4).length;
-        const waitlist = mockData.filter(job => job.status === 5).length;
-        const ghosted = tempGhosted.length;
-
-        return [reject, interview, offer, waitlist, ghosted];
+    const response = await fetch(`http://localhost:5000/api/emails?username=${user}`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
     }
+    const data = await response.json();
+    console.log(data);
+
+    // filtering fetched data into three main columns
+    const tempGood = data.filter((job) => job.status === 4);
+    setGoodStuff(tempGood);
+    let tempAwait = data.filter((job) => job.status === 1 || job.status === 3 || job.status === 5);
+    setAwaiting(tempAwait);
+
+    const currentDate = new Date();
+    const tempGhosted = mockData.filter((job) => {
+      const jobDate = new Date(job.date);
+      const diffTime = jobDate.getTime() - currentDate.getTime();
+      const diffDays = Math.abs(Math.round(diffTime / (1000 * 3600 * 24)));
+      console.log("Job ", job.id, ": ", diffDays);
+      return diffDays > 21;
+    });
+    setGhostedJobs(tempGhosted);
+    tempAwait = tempAwait.filter((job) => tempGhosted.includes(job) === false);
+    setAwaiting(tempAwait);
+    const tempBad = mockData.filter((job) => job.status === 2).concat(tempGhosted);
+    setBadStuff(tempBad);
+
+    const reject = mockData.filter((job) => job.status === 2).length;
+    const interview = mockData.filter((job) => job.status === 3).length;
+    const offer = mockData.filter((job) => job.status === 4).length;
+    const waitlist = mockData.filter((job) => job.status === 5).length;
+    const ghosted = tempGhosted.length;
+
+    return [reject, interview, offer, waitlist, ghosted];
+  };
   // 1. job application confirmation
   // 2. job application rejection
   // 3. interview invitation
@@ -69,18 +66,10 @@ function HomePage() {
     return [reject, interview, offer, waitlist, ghosted];
   };
 
-    useEffect(() => {
-        // TO DO: implement data fetching from b/e
-        const tempCount = fetchData();
-        console.log("Data Count: ", tempCount)
-        setDataCount(tempCount);
-
-    }, []);
-
   useEffect(() => {
     // TO DO: implement data fetching from b/e
-    fetchData();
-    const tempCount = countData(mockData);
+    const tempCount = fetchData();
+    console.log("Data Count: ", tempCount);
     setDataCount(tempCount);
   }, []);
 
