9754b52aeab60faa644652fdeb06776d64c1bcf4
Vince <112585869+vinn03@users.noreply.github.com>
2024-05-19 08:42:34 -0700

    added blue outline for new job updates

--- frontend/src/components/dashboard/page.jsx
+++ frontend/src/components/dashboard/page.jsx
@@ -16,7 +16,7 @@ function Dashboard({goodStuff, badStuff, awaiting}) {
                     {/* /*className=" m-auto h-fit flex flex-col bg-gray-300 w-11/12">(/) */}
                     <th className='w-full mb-5 bg-gray-900 rounded-md py-3 px-4 shadow-md'><h1 className="text-white">Awaiting Application</h1></th>
                         {awaiting.map(job => (
-                            <div className="mb-4 mx-2.5 w-11/12 border border-gray-300 bg-contain rounded-md shadow-md overflow-hidden" style={{backgroundColor: statusColours[job.status]}} key={job.id}>
+                            <div className={`mb-4 mx-2.5 w-11/12 border ${job.new ? 'border-blue-300' : 'border-gray-300'} bg-contain rounded-md shadow-md overflow-hidden`} style={{backgroundColor: statusColours[job.status]}} key={job.id}>
                                 <div className='flex-row flex'>
                                     <span className='h-full w-1/12' style={{backgroundColor: statusColours[job.status]}}>
                                     </span>
@@ -36,8 +36,8 @@ function Dashboard({goodStuff, badStuff, awaiting}) {
                     <td className='h-fit mb-auto bg-gray-200 rounded-md mx-2 flex-col flex justify-start w-full'>
                     <th className='w-full mb-5 bg-gray-900 rounded-md py-3 px-4 shadow-md'><h1 className="text-white">Good stuff</h1></th>
                         {goodStuff.map(job => (
-                            <div className="mb-4 mx-2.5 w-11/12 border border-gray-300 bg-contain rounded-md shadow-md overflow-hidden" style={{backgroundColor: statusColours[job.status]}} key={job.id}>
-                                <div className='flex-row flex'>
+                            <div className={`mb-4 mx-2.5 w-11/12 border ${job.new ? 'border-blue-300' : 'border-gray-300'} bg-contain rounded-md shadow-md overflow-hidden`} style={{backgroundColor: statusColours[job.status]}} key={job.id}>
+                            <div className='flex-row flex'>
                                     <span className='h-full w-1/12' style={{backgroundColor: statusColours[job.status]}}>
                                     </span>
                                     <span className="h-full w-11/12 bg-white"> 
@@ -57,9 +57,9 @@ function Dashboard({goodStuff, badStuff, awaiting}) {
                     <td className='h-fit bg-gray-200 rounded-md mx-2 flex-col flex justify-start w-full'>
                     <th className='w-full mb-5 bg-gray-900 rounded-md py-3 px-4 shadow-md'><h1 className="text-white"> Rejections</h1></th>
                         {badStuff.map(job => (
-                            <div className="mb-4 mx-2.5 w-11/12 border border-gray-300 bg-red-400 bg-contain rounded-md shadow-md overflow-hidden" key={job.id}>
-                                <div className='flex-row flex'>
-                                    <span className='h-full w-1/12 bg-red-400'>
+                        <div className={`mb-4 mx-2.5 w-11/12 border ${job.new ? 'border-blue-300' : 'border-gray-300'} bg-contain rounded-md shadow-md overflow-hidden`} style={{backgroundColor: statusColours[job.status]}} key={job.id}>
+                            <div className='flex-row flex'>
+                                <span className='h-full w-1/12 bg-red-400'>
                                     </span>
                                     <span className="h-full w-11/12 bg-white"> 
                                         <div className='flex flex-row px-3 py-2'>
