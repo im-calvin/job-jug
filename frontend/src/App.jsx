3190ec9a74d40269e6ff7cb4f3a5d35cd7719462
Vince <112585869+vinn03@users.noreply.github.com>
2024-05-18 22:58:24 -0700

    finalized dashboard data dipslay, attempting sankey

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/home" element={<HomePage />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
         </Routes>
       </BrowserRouter>
     </>
